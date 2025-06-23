0a24137bd390d6ab3b0a01078510a67d863210b3
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2025-06-23 23:37:58 +0200

    save & load cookies

--- xwalker/xwalker.go
+++ xwalker/xwalker.go
@@ -3,6 +3,7 @@ package xwalker
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 
@@ -12,6 +13,63 @@ import (
 type XWalker struct {
 	Playwright *playwright.Playwright
 	Page       playwright.Page
+}
+
+func LoginFromCookiesFile() (*XWalker, error) {
+	f, err := os.ReadFile("cookies.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	cookiez := strings.Split(string(f), "; ")
+	if len(cookiez) == 0 || (len(cookiez) == 1 && cookiez[0] == "") {
+		return nil, fmt.Errorf("no cookies found in file")
+	}
+
+	var cookies []playwright.OptionalCookie
+	for _, cookie := range cookiez {
+		if cookie == "" {
+			continue // Skip empty cookies
+		}
+		parts := strings.SplitN(cookie, "=", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid cookie format: %s", cookie)
+		}
+		name := parts[0]
+		value := parts[1]
+		cookies = append(cookies, playwright.OptionalCookie{
+			Name:  name,
+			Value: value,
+			URL:   playwright.String("https://x.com"), // Optional: specify the domain if needed
+		})
+	}
+	pw, err := playwright.Run()
+	if err != nil {
+		return nil, err
+	}
+	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
+		Headless: playwright.Bool(false),
+		Timeout:  playwright.Float(0), // Set a timeout for launching the browser
+	})
+	if err != nil {
+		return nil, err
+	}
+	context, err := browser.NewContext(playwright.BrowserNewContextOptions{
+		StorageState: &playwright.OptionalStorageState{
+			Cookies: cookies,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	page, err := context.NewPage()
+	if err != nil {
+		return nil, err
+	}
+	return &XWalker{
+		Playwright: pw,
+		Page:       page,
+	}, nil
 }
 
 func LoginX(email, pass, user string) (*XWalker, error) {
@@ -127,3 +185,34 @@ func (x *XWalker) OpenFollowersPageAndUnsubN(n int) error {
 }
 
 // TODO: handle if at some point the page changes, restart the process few times - then notify me
+
+func (x *XWalker) StoreCookiesToFile() error {
+	cookies, err := x.Page.Context().Cookies()
+	if err != nil {
+		return err
+	}
+
+	// Convert cookies to a string format or save them to a file
+	cookieData := ""
+	for _, cookie := range cookies {
+		cookieData += fmt.Sprintf("%s=%s; ", cookie.Name, cookie.Value)
+	}
+
+	f, err := os.Create("cookies.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.WriteString(cookieData)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (x *XWalker) RefuseAllCookies() {
+	// Click the "Refuse" button for cookies if it exists
+	if err := x.Page.Click("button:has-text('Refuse non-essential cookies')"); err != nil {
+	}
+	time.Sleep(time.Second + time.Duration(rand.Intn(150))*time.Millisecond) // Wait for the action to complete
+}
